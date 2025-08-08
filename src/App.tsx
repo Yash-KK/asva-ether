141f1c08a457c72f1cb86c33732bd6def90437cf
Yash-KK <reachyashk@gmail.com>
2025-08-08 13:03:26 +0530

    1-connect-wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import { WagmiProvider } from "wagmi";
+import { useAccount, WagmiProvider } from "wagmi";
 import { config } from "../config.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { WalletOptions } from "@/components/Profile";
+import { Account } from "./components/Account.tsx";
+import { WalletOptions } from "./components/WalletOptions.tsx";
 
 const queryClient = new QueryClient();
+
+function ConnectWallet() {
+  const { isConnected } = useAccount();
+  if (isConnected) return <Account />;
+  return <WalletOptions />;
+}
 
 function App() {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <WalletOptions />
+        <div className="bg-black h-screen">
+          <ConnectWallet />
+        </div>
       </QueryClientProvider>
     </WagmiProvider>
   );
